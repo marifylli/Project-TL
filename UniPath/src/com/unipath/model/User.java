01d5f0cbd1389fd690b7ae4d248215445e35a389
nikos <nikosm915@gmail.com>
2026-05-14 15:38:05 +0300

    added user class outline

---
+++ UniPath/src/com/unipath/model/User.java
@@ -0,0 +1,11 @@
+package com.unipath.model;
+
+public class User {
+    private String userId;
+    private String userame;
+    private String passwordHash;
+    private String email;
+    private String firstName;
+    private String lastName;
+    
+}

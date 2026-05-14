a401b03e78eb68ffc963445ab49ec062d920210a
nickipanagaki <up1108370@ac.upatras.gr>
2026-05-14 17:24:29 +0300

    add DBManager sqlite connection

--- UniPath/src/com/unipath/dataBase/DBManager.java
+++ UniPath/src/com/unipath/dataBase/DBManager.java
@@ -1,4 +1,46 @@
 package com.unipath.dataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBManager {
+
+    private static final String DB_URL = "jdbc:sqlite:unipath.db";
+    private static DBManager instance;
+    private Connection connection;
+
+    // Singleton - μόνο ένα DBManager υπάρχει
+    private DBManager() {
+    }
+
+    public static DBManager getInstance() {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    // Άνοιγμα σύνδεσης
+    public Connection connect() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(DB_URL);
+            System.out.println(" SQLite Successfully connected");
+        }
+        return connection;
+    }
+
+    // Κλείσιμο σύνδεσης
+    public void disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("SQLite Disconnected!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Σφάλμα: " + e.getMessage());
+        }
+    }
+
 }

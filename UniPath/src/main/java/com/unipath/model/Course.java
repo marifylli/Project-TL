1411828241193efe3831d7b8a042642f31e769cf
MARIA ELENI SKONDRA <up1108330@ac.upatras.gr>
2026-05-15 16:08:41 +0300

    fix changes

--- UniPath/src/main/java/com/unipath/model/Course.java
+++ UniPath/src/main/java/com/unipath/model/Course.java
@@ -1,4 +1,34 @@
 package com.unipath.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Course {
+    public int courseID;
+    private String title;
+    private String description;
+    private int ects;
+    private int semester;
+    private boolean groupA;
+    private boolean groupB;
+    private List<String> directions;
+    private boolean isActive;
+    private float averageRating;
+    private float workloadScore;
+    private int workloadRank;
+    private Date lastModifiedDate;
+    private String lastModifiedBy;
+    private String rules;
+    private String prerequisites;
+
+
+
+    public Course() {
+
+    }
+    public void getECTS() {
+
+    }
+
+
 }

2ef36ef3b63f892870502a74032143d49cc75704
ioanna <ioannaalexiou593@gmail.com>
2026-05-19 16:16:34 +0300

    evaluation use case 2 update

--- UniPath/src/main/java/com/unipath/controller/EvaluationControllerUC2.java
+++ UniPath/src/main/java/com/unipath/controller/EvaluationControllerUC2.java
@@ -1,4 +1,50 @@
 package com.unipath.controller;
 
-public class EvaluationController {
+import com.unipath.model.CourseEvaluation;
+import com.unipath.model.Course;
+import java.util.List;
+
+
+public class EvaluationControllerUC2 {
+
+
+    // Methods από το class diagram
+    public void create() {
+        // TODO
+    }
+
+
+    public int getStudentId() {
+        return 0; // TODO
+    }
+
+
+    public List<Course> queryCoursesAttended(int studentId) {
+        return null; // TODO
+    }
+
+
+    public void courseSelected(int courseId) {
+        // TODO
+    }
+
+
+    public boolean checkEvaluationRight(int studentId, int courseId) {
+        return false; // TODO
+    }
+
+
+    public boolean checkFields() {
+        return false; // TODO
+    }
+
+
+    public void saveEvaluation(CourseEvaluation evaluation) {
+        // TODO
+    }
+
+
+    public void updateCourseStats(int courseId) {
+        // TODO
+    }
 }

bdb226ad53f724578bb54e81e0e35613e26edf9f
NIKOLAOS MICHAIL <nikosm915@gmail.com>
2026-05-20 16:51:02 +0300

    getters setters for course

--- UniPath/src/main/java/com/unipath/model/Course.java
+++ UniPath/src/main/java/com/unipath/model/Course.java
@@ -26,23 +26,52 @@ public class Course {
     public Course() {
 
     }
-    public int getECTS() {
+    public String getCourseID() { return courseID; }
+    public void setCourseID(String courseID) { this.courseID = courseID; }
 
-    return ects;}
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public boolean isGroupA() {
-        return groupA;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public boolean isGroupB() {
-        return groupB;
-    }
+    public int getEcts() { return ects; }
+    public void setEcts(int ects) { this.ects = ects; }
 
-    public String getDirections() {
-        return directions;
-    }
-    public void setDirections(String directions){
-        this.directions=directions;
-    }
+    public int getSemester() { return semester; }
+    public void setSemester(int semester) { this.semester = semester; }
 
+    public boolean isGroupA() { return groupA; }
+    public void setGroupA(boolean groupA) { this.groupA = groupA; }
+
+    public boolean isGroupB() { return groupB; }
+    public void setGroupB(boolean groupB) { this.groupB = groupB; }
+
+    public String getDirections() { return directions; }
+    public void setDirections(String directions) { this.directions = directions; }
+
+    public boolean isActive() { return isActive; }
+    public void setActive(boolean isActive) { this.isActive = isActive; }
+
+    public float getAverageRating() { return averageRating; }
+    public void setAverageRating(float averageRating) { this.averageRating = averageRating; }
+
+    public float getWorkloadScore() { return workloadScore; }
+    public void setWorkloadScore(float workloadScore) { this.workloadScore = workloadScore; }
+
+    public int getWorkloadRank() { return workloadRank; }
+    public void setWorkloadRank(int workloadRank) { this.workloadRank = workloadRank; }
+
+    public Date getLastModifiedDate() { return lastModifiedDate; }
+    public void setLastModifiedDate(Date lastModifiedDate) { this.lastModifiedDate = lastModifiedDate; }
+
+    public String getLastModifiedBy() { return lastModifiedBy; }
+    public void setLastModifiedBy(String lastModifiedBy) { this.lastModifiedBy = lastModifiedBy; }
+
+    public String getRules() { return rules; }
+    public void setRules(String rules) { this.rules = rules; }
+
+    public String getPrerequisites() { return prerequisites; }
+    public void setPrerequisites(String prerequisites) { this.prerequisites = prerequisites; }
 }
+
